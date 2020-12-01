2f3d382d6e91bdfb6d1e2a198674138888be233c
mark-bak <markbak7@gmail.com>
2020-12-01 22:19:13 +0000

    Slight cleanup of link length behaviour

--- KivyWidgets/links.py
+++ KivyWidgets/links.py
@@ -19,6 +19,9 @@ from kivy.lang.builder import Builder
 Builder.load_file("KivyWidgets/links.kv")
 
 class Link(Widget):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.update_length()
 
     a = ObjectProperty(None)
     b = ObjectProperty(None)
@@ -29,13 +32,14 @@ class Link(Widget):
 
     def update_length(self):
         new_len = float(np.linalg.norm([self.a.x-self.b.x,self.a.y-self.b.y]))
+        new_mid = self.a.x+(self.b.x-self.a.x)/2,self.a.y+(self.b.y-self.a.y)/2
         self.length = new_len
-        self.midpoint = self.a.x+(self.b.x-self.a.x)/2,self.a.y+(self.b.y-self.a.y)/2
+        self.midpoint = new_mid
         if self.parent != None:
-            for w in self.parent.walk():
-                if isinstance(w,LinkData):
-                    if w.ref == self.ref:
-                        w.len_txt = str(round(new_len,2))
+           for w in self.parent.walk():
+               if isinstance(w,LinkData):
+                   if w.ref == self.ref:
+                       w.len_txt = str(round(new_len,2))
         return new_len
 
     
