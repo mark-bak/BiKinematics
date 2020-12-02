e6dbafa6ef73f62660b6a31e865050c294a9c231
mark-bak <markbak7@gmail.com>
2020-12-02 14:47:32 +0000

    cleaning up horrible loops to update links&data

--- KivyWidgets/points.py
+++ KivyWidgets/points.py
@@ -18,52 +18,37 @@ from kivy.lang.builder import Builder
 
 Builder.load_file("KivyWidgets/points.kv")
 
+class Point(Scatter):
+    ##Draggable point of bike geo
+
+    #Properties
+    name = StringProperty()
+    point_type = StringProperty()
+    point_data = ObjectProperty(None)
+
+    def on_touch_down(self,touch):
+        #custom touch behaviour
+        if self.collide_point(touch.x,touch.y):
+            print('touch '+self.name)
+            if self.parent.mode == 'Add_Link': #if in Add_Link mode, add this to the selection list
+                self.parent.link_points.append(self)
+            if self.parent.mode =='Del_Point':
+                self.parent.delete_point(self)
+        return super(Point,self).on_touch_down(touch) #do standard scatter touch behaviour
+
 class PointData(BoxLayout):
     ##Coordinate box - layout in .kv file
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
 
     #Properties
+    point = ObjectProperty(None)
     txt_x = StringProperty()
     txt_y = StringProperty()
-    ref = StringProperty()
+    name = StringProperty()
     point_type = StringProperty()
 
     def type_dropdown(self):
         #might need this
         pass
 
-class Point(Scatter):
-    ##Draggable point of bike geo
-
-    #Properties
-    ref = StringProperty()
-    point_type = StringProperty('free')
-
-    def on_pos(self,x,y):
-        self.update_coords()
-
-    def update_coords(self):
-        ##Update the associated coordinate display and links
-        if self.parent != None:
-            for w in self.parent.walk():
-                if isinstance(w,PointData):
-                    if w.ref == self.ref:
-                        w.txt_x = str(round(self.x))
-                        w.txt_y = str(round(self.y))
-                if isinstance(w,Link): 
-                    if w.a == self:
-                        w.points[0] = self.pos
-                    if w.b == self:
-                        w.points[1] = self.pos
-                    w.update_length()
-
-    def on_touch_down(self,touch):
-        #custom touch behaviour
-        if self.collide_point(touch.x,touch.y):
-            print('touch '+self.ref)
-            if self.parent.mode == 'Add_Link': #if in Add_Link mode, add this to the selection list
-                self.parent.link_points.append(self)
-            if self.parent.mode =='Del_Point':
-                self.parent.delete_point(self)
-        return super(Point,self).on_touch_down(touch) #do standard scatter touch behaviour
