9bc4c95feb39654005619e124676514d5125afaa
MB <markbak7@gmail.com>
2021-02-15 13:44:35 +0000

    added drawing for chainring

--- KivyWidgets/components.py
+++ KivyWidgets/components.py
@@ -1,11 +1,15 @@
 
+import numpy as np
+
 #Custom Imports
 #pylint: disable=import-error
 from KivyWidgets.links import Link,LinkData
+from kivy.uix.widget import Widget
 
 #Kivy Properties
 #pylint: disable=no-name-in-module
 from kivy.properties import ObjectProperty
+from kivy.properties import NumericProperty
 
 #Kivy Language Tools
 from kivy.lang.builder import Builder
@@ -20,3 +24,18 @@ class Shock(Link):
 
 class ShockData(LinkData):
     pass
+
+class Cog(Widget):
+    centrepoint = ObjectProperty(None)
+    diameter_ref = ObjectProperty(None)
+    diameter = NumericProperty(69)
+    mp = ObjectProperty(None)
+
+    def teeth_to_dia(self,str_teeth):
+        if not str_teeth or self.mp.px_to_mm == 0:
+            return 0
+        else:               
+            link_len = 12.7
+            n_teeth = float(str_teeth)
+            dia = link_len / np.sin( np.pi / n_teeth ) *  (1/self.mp.px_to_mm)
+            return float(dia)
